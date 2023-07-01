0beec5fa3cba4689314e0c52f16346b2fc3efce4
Noah Razzaq <nrazzaq@vinatis.com>
2023-07-01 17:14:29 +0200

    fix: remove vercel edge dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 import { defineConfig } from 'astro/config';
-import vercelEdge from '@astrojs/vercel/edge';
 import { inject } from '@vercel/analytics';
-
 
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    adapter: vercelEdge(),
 });
 
 inject();
