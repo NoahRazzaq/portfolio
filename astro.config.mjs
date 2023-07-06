4d4c978db4bad34ae174fcabe70bc0cd14ec964c
Noah Razzaq <nrazzaq@vinatis.com>
2023-07-06 15:24:50 +0200

    :bug: adding vercel analytics config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import { inject } from '@vercel/analytics';
+import vercel from '@astrojs/vercel/static';
 
 
 // https://astro.build/config
 export default defineConfig({
+    adapter: vercel({
+    analytics: true
+      })
 });
 
 inject();
